677f80c9ea2c43a02c042cdbd466753e539a0494
camilatschinkel <camilatschinkelrodrigues@gmail.com>
2025-03-29 13:18:37 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let video;
+let canvas;
+let filterImage;
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight); // Ajusta o tamanho do canvas ao tamanho da tela
+  canvas.parent('p5-canvas-container');
+
+  video = createCapture(VIDEO);
+  console.log("Câmera iniciada com sucesso!");
+  video.size(windowWidth, windowHeight); // Ajusta o tamanho do vídeo ao tamanho da tela
+  video.hide();
+
+  filterImage = loadImage('https://i.postimg.cc/k5PckJLb/85130723-c7a6-4d13-91a8-f4788a95c5ce-20250316-154127-0000.png');
+}
+
+function draw() {
+  background(220);
+  image(video, 0, 0, windowWidth, windowHeight); // Ajusta o tamanho da imagem ao tamanho da tela
+
+  image(filterImage, 0, 0, windowWidth, windowHeight); // Ajusta o tamanho do filtro ao tamanho da tela
+}
+
+
+function takeSnapshot() {
+  save(filename);
+}
+
+function takeSnapshot() {
+  const nomeArquivo = gerarNomeArquivo(); // Chama a função para obter o nome do arquivo
+  save(nomeArquivo); // Usa o nome do arquivo retornado pela função
+}
